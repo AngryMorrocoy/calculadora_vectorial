4c2c8f03c0f724ba12faf2086424cef8f72eda8b
AngryMorrocoy <mrivasmahecha@gmail.com>
2024-01-23 13:44:36 -0400

    Small redesign to operational keyboard

--- src/components/keyboards/OperationalKeyboard.jsx
+++ src/components/keyboards/OperationalKeyboard.jsx
@@ -1,28 +1,30 @@
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { OPERATION_INSERTION } from "../constraints";
 
 const OperationalKeyboard = ({ }) => {
   return (
     <ButtonGroup vertical className="me-2 w-25">
       <ButtonGroup>
+        <Button variant="secondary">Vectores</Button>
+      </ButtonGroup>
+      <ButtonGroup>
         <Button variant="secondary">sen</Button>
         <Button variant="secondary">cos</Button>
       </ButtonGroup>
       <ButtonGroup>
-        <Button variant="secondary">(</Button>
-        <Button variant="secondary">)</Button>
+        <Button variant="secondary">sq</Button>
+        <Button variant="secondary">a²</Button>
       </ButtonGroup>
       <ButtonGroup>
         <Button variant="secondary">•</Button>
         <Button variant="secondary">X</Button>
       </ButtonGroup>
       <ButtonGroup>
-        <Button variant="secondary">+</Button>
+        <Button variant="secondary" insertiontype={OPERATION_INSERTION}>
+          +
+        </Button>
         <Button variant="secondary">-</Button>
-      </ButtonGroup>
-      <ButtonGroup>
-        <Button variant="secondary">sq</Button>
-        <Button variant="secondary">a²</Button>
       </ButtonGroup>
     </ButtonGroup>
   );
