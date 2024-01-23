567c3e76cdde57c3d84fdcd704e44561098c648e
AngryMorrocoy <mrivasmahecha@gmail.com>
2024-01-23 13:35:37 -0400

    Created class to interact with Vectors

---
+++ src/objects/Vector.js
@@ -0,0 +1,35 @@
+export default class Vector {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z ? z : 0;
+  }
+
+  dotProduct(vectorB) {
+    return vectorB.x * this.x + vectorB.y * this.y + vectorB.z * this.z;
+  }
+
+  crossProduct(vectorB) {
+    return new Vector(
+      this.y * vectorB.z - this.z * vectorB.y,
+      this.z * vectorB.x - this.x * vectorB.z,
+      this.x * vectorB.y - this.y * vectorB.x
+    );
+  }
+
+  add(vectorB) {
+    return new Vector(
+      this.x + vectorB.x,
+      this.y + vectorB.y,
+      this.z + vectorB.z
+    );
+  }
+
+  sub(vectorB) {
+    return new Vector(
+      this.x - vectorB.x,
+      this.y - vectorB.y,
+      this.z - vectorB.z
+    );
+  }
+}
