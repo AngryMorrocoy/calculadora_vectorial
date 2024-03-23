76adf374fc030a9f3d92d93750003d8b9a7d22b0
AngryMorrocoy <mrivasmahecha@gmail.com>
2024-03-22 22:18:59 -0400

    Remodeled card

--- src/components/VectorCard.jsx
+++ src/components/VectorCard.jsx
@@ -1,4 +1,5 @@
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -22,26 +23,38 @@ const VectorCard = ({ name, vector }) => {
   };
 
   return (
-    <Card className="w-100 mb-3">
-      <Card.Body>
-        <Card.Title className="text-center fw-bold">{name}</Card.Title>
+    <>
+      <YesNoModal show={displayModal} onHide={() => setDisplayModal(false)} title={`¿Seguro que deseas eliminar el vector "${name}"?`} onYes={dispatchDeleteVector} onNo={(() => setDisplayModal(false))} />
+      <Card className="w-100 mb-3">
+        <Card.Body>
+          <Card.Title className="text-center fw-bold w-100 m-0 p-0">
+            <Dropdown className="w-100" as={ButtonGroup}>
+              <Button className="w-75" variant="outline-secondary fw-bold fs-5 p-0">{name}</Button>
+              <Dropdown.Toggle split variant="secondary" />
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => setDisplayModal(true)}>Eliminar</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Card.Title>
 
-        <Row className="pt-3">
-          <Col className="text-center w-100 fw-bold">X</Col>
-          <Col className="text-center w-100 fw-bold">Y</Col>
-          <Col className="text-center w-100 fw-bold">Z</Col>
-        </Row>
-        <Row className="">
-          <Col className="text-center w-100 border border-secondary">{vector.x}</Col>
-          <Col className="text-center w-100 border border-secondary">{vector.y}</Col>
-          <Col className="text-center w-100 border border-secondary">{vector.z}</Col>
-        </Row>
+          <Row className="pt-3">
+            <Col className="text-center w-100 fw-bold">X</Col>
+            <Col className="text-center w-100 fw-bold">Y</Col>
+            <Col className="text-center w-100 fw-bold">Z</Col>
+          </Row>
+          <Row className="">
+            <Col className="text-center w-100 border border-secondary">{vector.x}</Col>
+            <Col className="text-center w-100 border border-secondary">{vector.y}</Col>
+            <Col className="text-center w-100 border border-secondary">{vector.z}</Col>
+          </Row>
 
-        <Card.Text className="border-top border-2 border-secondary w-100 mt-4 pt-2">
-          Norma: <span className="fw-bold">{vector.size}</span>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+          <Card.Text className="border-top border-2 border-secondary w-100 mt-4 pt-2">
+            Norma: <span className="fw-bold">{vector.size}</span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+    </>
   )
 };
 
